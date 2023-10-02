1e443f8e341872eaf11c61a695f656d5ebe0bf3b
Andreas Stenius <git@astekk.se>
2023-10-02 00:16:58 -0400

    Copy deck to clipboard.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,36 @@
 <script>
 	import cards from '$lib/assets/cards.json';
 	import CardPicker from '$lib/CardPicker.svelte';
+	import Button from '$lib/Button.svelte';
 
 	let selected = [];
 	$: num_cards = selected.reduce((count, s) => count + s.count, 0);
+
+	function get_deck_txt() {
+		return selected.map((item) => `${item.count}x ${item.card.name}`).join('\n');
+	}
+
+	let copy_msg = undefined;
+	function copy_deck_to_clipboard() {
+		navigator.clipboard.writeText(get_deck_txt()).then(
+			() => (copy_msg = 'Deck copied to clipboard!'),
+			(e) => (copy_msg = `Sorry, copy deck to clipboard failed /: ${e}`)
+		);
+	}
 </script>
 
 <h1>Algomancy Deck Builder</h1>
 
 <div>
 	Deck: {num_cards} card{num_cards !== 1 ? 's' : ''}
+	<div>
+		<Button on:click={copy_deck_to_clipboard}>Copy deck to clipboard</Button>
+	</div>
+	{#if copy_msg}
+		<div>
+			{copy_msg}
+		</div>
+	{/if}
 </div>
 
 <CardPicker {cards} bind:selected />
