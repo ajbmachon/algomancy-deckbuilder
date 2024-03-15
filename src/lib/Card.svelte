9a88ea3b72f43b5a4c181930047195727f61885a
Andreas Stenius <git@astekk.se>
2024-03-15 11:25:21 +0100

    animate card size rather than image position

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -4,14 +4,10 @@
   export let name;
   export let image_name;
   export let faction;
-  export let height = 400;
-
-  $: height_var = `${height}px`;
 </script>
 
 <img
   class={faction}
-  style:--height={height_var}
   src="{base}/card_images/{image_name}"
   alt={name}
   on:click
@@ -19,12 +15,14 @@
 
 <style>
   img {
-    height: var(--height, 400px);
+    width: 288px;
+    height: 390px;
     border-radius: 16px;
     box-shadow: 10px 10px 10px -5px hsl(0deg 0% 0% / 60%);
-    object-position: 50% -10px;
+    object-fit: cover;
+    object-position: bottom;
 
-    transition: transform 0.25s ease-out, box-shadow 0.75s ease-out, object-position 0.1s ease-in;
+    transition: transform 0.25s ease-out, box-shadow 0.75s ease-out, height 0.2s ease-out;
 
     user-drag: none;
     -webkit-user-drag: none;
@@ -34,10 +32,10 @@
     -ms-user-select: none;
   }
   img:hover {
+    height: 400px;
     box-shadow: 0px 0px 15px 5px hsl(0deg 0% 50% / 80%), 10px 10px 10px -5px hsl(0deg 0% 0% / 60%);
-    object-position: 50% 0px;
     transform: translate(5px, -5px) perspective(5000px) rotateX(10deg) rotateY(0deg) rotateZ(-1deg);
-    transition: transform 0.5s ease-out, box-shadow 0.2s ease-in, object-position 0.15s ease-out;
+    transition: transform 0.5s ease-out, box-shadow 0.2s ease-in, height 0.2s ease-out;
   }
   img.earth:hover {
     box-shadow: 0px 0px 4px 3px hsl(220deg 8% 86% / 70%), 0px 0px 15px 5px hsl(28deg 69% 57% / 90%),
