2b8c01d63de8086e62b5df9e364978391f6030a9
Andreas Stenius <git@astekk.se>
2024-03-15 12:54:30 +0100

    tweak layout rules..

--- src/lib/components/CardList.svelte
+++ src/lib/components/CardList.svelte
@@ -5,21 +5,20 @@
 
   const dispatch = createEventDispatcher();
   const click_event = (entry) => () => dispatch('click', entry);
+  const height = 400;
 
   export let list;
   export let crossfade;
-  export let height = undefined;
   export let card_splay = 40;
 
   $: [send, receive] = crossfade;
-  $: total_list_height = 32 + height + Math.max(0, list.length - 1) * card_splay;
 </script>
 
-<div class="relative" style:height="{total_list_height}px">
+<div class="relative card-list">
   {#each list as entry, idx (entry.id)}
     <div
       class="absolute card-slot"
-      style:top={`${idx * 40}px`}
+      style:top={`${idx * card_splay}px`}
       in:receive={{ key: entry.id }}
       out:send={{ key: entry.id }}
       animate:flip={{ duration: 500 }}
@@ -29,13 +28,21 @@
         image_name={entry.card.image_name}
         faction={entry.card.factions[0]}
         on:click={click_event(entry)}
-        bind:height
       />
     </div>
   {/each}
 </div>
 
 <style>
+  .card-list {
+    height: calc(100vh - 300px);
+    overflow-y: scroll;
+  }
+
+  .card-slot :global(img) {
+    margin: 20px;
+  }
+
   .card-slot:hover {
     z-index: 1;
   }
